fa9c6af8ba169c64fd18759e83f9b341786b87a6
gaizkazaqhariou <muhammad_zaqhariou_24rpl@student.smktelkom-mlg.sch.id>
2017-09-05 10:01:26 +0700

    ini transaksi

---
+++ kontrak.php
@@ -0,0 +1,45 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Kontrak extends MY_Controller {
+    public function __construct() {
+        parent::__construct();
+        parent::default_meta();
+        $this->data->metadata = $this->template->get_metadata();
+		$this->data->judul=$this->template->get_judul();
+		$this->load->model(array('kontrak_m'));
+    }
+    
+    public function index() {
+
+		if (!$this->autentifikasi->sudah_login()) redirect (site_url('login'));
+		redirect(site_url('main'));
+    }
+	
+	public function formKontrak(){
+		$data['data']=$this->kontrak_m->getKodeKontrak();
+		$this->load->view('admin/master/formKontrak',$data);
+	}
+	
+	public function getListKontrak(){
+		$data['rows']=$this->kontrak_m->getListKontrak('rows');
+		$data['total']=$this->kontrak_m->getListKontrak('total');
+		echo json_encode($data);
+		
+	}
+	
+	public function simpanKontrak(){
+		$data=$this->kontrak_m->simpanKontrak();
+		echo json_encode($data);
+	}
+	
+	public function hapusKontrak(){
+		$data=$this->kontrak_m->hapusKontrak();
+		echo json_encode($data);
+	}
+	
+	
+
+	
+	
+	
+    
+}
