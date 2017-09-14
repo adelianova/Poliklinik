be77fa9545ea73a6bc890a5d53918eb919c49653
adelianova <9a.adelianova@gmail.com>
2017-09-14 08:18:18 +0700

    RETUR CRD DONE!!!!

---
+++ application/views/transaksi/formRetur.php
@@ -0,0 +1,155 @@
+	<form id="form_retur" method="post" novalidate>
+				<input type='hidden' name='edit' id='edit' value=''/>
+				<table width='350px' class='dialog-form' >
+					<tr>
+						<td class='label_form'>ID Retur</td>
+						<td >
+							<input name='id_retur' id='id_retur' readonly='true' 
+							class='easyui-validatebox textbox' style="padding:3px;width:90%" value="<?php echo $data['id_retur'];?>"/>	
+						</td>
+					</tr>
+					<tr>
+						<td class='label_form'>NO Retur</td>
+						<td >
+							<input name='no_retur' id='no_retur' class='easyui-validatebox textbox' required="true" type="text" style="padding:3px;width:90%"/>
+						</td>
+					</tr>
+					<tr>
+						<td class='label_form'>Tanggal</td>
+						<td >
+							<input name='tgl' id='tgl' class='easyui-datetimebox' style="padding:3px;width:90%"/>	
+						</td>
+					</tr>
+					<tr>
+						<td class='label_form'>Petugas</td>
+						<td >
+							<input name='petugas' id='petugas' class='easyui-validatebox textbox' required="true" type="text" style="padding:3px;width:90%"/>
+						</td>
+					</tr>
+			</table>
+			<div id="dialog-m_tambah" class="easyui-dialog" style="width:410px; height:250px; padding: 10px 20px" 
+					closed="true" iconCls="icon-user">
+					</div>
+			<table id="datagrid-m_detail" title="" class="easyui-datagrid scrollbarx" 
+					style="width:auto; height: auto;" 
+					data-options="
+					url:'<?php echo base_url().'index.php/retur/getListDetail';?>',
+					toolbar:'#toolbar2',rownumbers:true,pagination:true,border:false,
+					striped:true,fit:true,fitColumns:true,
+					singleSelect:true,collapsible:false">
+						<thead>
+							<tr>
+								<th field="id_dtl_retur" width="100" sortable="true">ID DETAIL RETUR</th>
+								<th field="id_retur" width="100" sortable="true">ID RETUR</th>
+								<th field="id_dtl_stock" width="100" sortable="true">ID DETAIL STOCK</th>
+								<th field="qty" width="100" sortable="true">QUANTITY</th>
+								<th field="keterangan" width="100" sortable="true">KETERANGAN</th>
+							</tr>
+						</thead>
+					</table>
+					<div id="toolbar2" style='padding:5px;height:25px'>
+						<div style="display:inline;">
+						<a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-ok'" onClick="iniTambah()">Tambah</a>&nbsp;
+						<a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-edit'" onClick="editTambah()">Edit</a>&nbsp;
+			        	<a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-remove'" onClick="removeTambahRetur()">Remove</a>&nbsp;
+							 
+					</div>
+		</form>
+	
+<script type="text/javascript">
+		$(field="ID_DETAIL_RETUR").each(function(){
+			$(this).hide()
+			console.log(this);
+		})
+function iniTambah(){
+	var id_retur = $('#datagrid-m_retur').datagrid('getSelected');
+		$('#dialog-m_tambah').dialog({ 
+		    closed: false, 
+			cache: false, 
+			modal: true, 
+			href:base_url+'index.php/retur/formTambahRetur/'+id_retur.id_retur,
+			title:'Tambah Obat',
+			onLoad:function(){
+				$('#form_tambah_retur').form('clear');
+				$('#form_tambah_retur #edit').val('');
+			},
+			onClose: function(){
+				
+			 $('#dialog-m_detail').datagrid('reload');
+			}
+			});
+	}
+function editTambah(){
+        var row = $('#datagrid-m_detail').datagrid('getSelected');
+        console.log(row);
+		if(row){
+		$('#dialog-m_tambah').dialog({ 
+		    closed: false, 
+			cache: false, 
+			modal: true, 
+			href:base_url+'index.php/retur/formTambahRetur',
+			title:'Edit Obat',
+			onLoad:function(){
+				$('#form_tambah_retur').form('clear');
+				$('#form_tambah_retur #edit').val('1');
+				$('#form_tambah_retur').form('load',row);	
+			}
+			});
+		}else{
+			$.messager.alert('INFO','Pilih satu record dulu','info');
+		}
+	}
+	function editRetur(){
+		
+        var row = $('#datagrid-m_retur').datagrid('getSelected');
+        console.log(row);
+		if(row){
+		
+		$('#dialog-m_retur').dialog({ 
+		    closed: false, 
+			cache: false, 
+			modal: true, 
+			href:base_url+'index.php/retur/formRetur',
+			title:'Edit Obat',
+			onLoad:function(){
+				
+				
+				$('#form_retur').form('clear');
+				$('#form_retur #edit').val('1');
+				$('#form_retur').form('load',row);	
+				$('#datagrid-m_detail').datagrid('load',{id_retur:row.id_retur});
+				
+			}
+			});
+		}else{
+			$.messager.alert('INFO','Pilih satu record dulu','info');
+		}
+		
+	}
+
+
+function removeTambahRetur(){
+		var row = $('#datagrid-m_detail').datagrid('getSelected');
+		if (row){
+			$.messager.confirm('Konfirmasi', 'Anda yakin menghapus data ini',function(z){
+				if (z){
+					$.post('<?php echo site_url('retur/removeTambahRetur'); ?>',{id_dtl_retur:row.id_dtl_retur},function(result){
+						console.log(result)
+						if (!result.error){
+							$('#datagrid-m_detail').datagrid('reload');
+							$.messager.alert('INFO',result.msg,'info');
+							} else {
+							$.messager.alert('INFO',result.msg,'info');
+						}
+					},'json');
+				}
+			});
+			}else{
+			$.messager.alert('INFO','Pilih satu record dulu','info');
+		}
+	}
+
+</script>
+
+
+	
---
+++ application/views/transaksi/retur.php
@@ -0,0 +1,150 @@
+	
+<table id="datagrid-m_retur" title="" class="easyui-datagrid scrollbarx" 
+		style="width:auto; height: auto;" 
+		data-options="
+		url:'<?php echo base_url().'index.php/retur/getListRetur';?>',
+		toolbar:'#toolbar',rownumbers:true,pagination:true,border:false,
+		striped:true,fit:true,fitColumns:true,
+		singleSelect:true,collapsible:false">
+			<thead>
+				<tr>
+					<th field="id_retur" width="100" sortable="true">ID RETUR</th>
+					<th field="no_retur" width="100" sortable="true">NO RETUR</th>
+					<th field="tgl" width="100" sortable="true">TANGGAL</th>
+					<th field="petugas" width="100" sortable="true">PETUGAS</th>
+				</tr>
+			</thead>
+		</table>
+		
+		<div id="toolbar" style='padding:5px;height:25px'>
+			<div style="display:inline;">
+			<a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-add'" onClick="addRetur()">Add</a>&nbsp;
+			<a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-edit'" onClick="editRetur()">Edit</a>&nbsp;
+        	<a href="javascript:void(0)" class="easyui-linkbutton" data-options="iconCls:'icon-remove'" onClick="removeRetur()">Remove</a>&nbsp;
+				 
+			</div>
+			<div style="display:inline;float:right;padding-top:-10px;">
+				<input id="sretur" class="easyui-searchbox" style="width:250px" 
+				searcher="cariRetur" prompt="Ketik disini" menu="#muser"></input>  
+				<div id="muser" style="width:150px"> 
+					<div name='id_retur'>ID</div>
+					<div name='tgl'>Tanggal</div>
+					<div name='no_retur'>No Retur</div>
+					<div name='petugas'>Petugas</div>
+				</div>  
+			</div>
+		</div>
+		<div id="dialog-m_retur" class="easyui-dialog" style="width:800px; height:500px; padding: 10px 20px" 
+		closed="true" buttons="#dialog-buttons" iconCls="icon-user">
+		</div>
+		<!-- Dialog Button -->
+	<div id="dialog-buttons">
+		<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-ok" onclick="simpanRetur();">Simpan</a>
+		<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:jQuery('#dialog-m_retur').dialog('close');">Batal</a>
+	</div>
+		
+<<script>
+	
+	
+	function cariRetur(value,name){
+		
+		$('#datagrid-m_retur').datagrid('load', { "searchKey": name, "searchValue": value });
+	}
+	
+	function addRetur(){
+		$('#dialog-m_retur').dialog({ 
+		    closed: false, 
+			cache: false, 
+			modal: true, 
+			href:base_url+'index.php/retur/formRetur',
+			title:'Tambah Transaksi Retur Obat',
+			onLoad:function(){
+				
+				$('#form_retur').form('clear');
+				$('#form_retur #edit').val('');
+			}
+			});
+	}
+	
+	function editRetur(){
+		
+        var row = $('#datagrid-m_retur').datagrid('getSelected');
+        console.log(row);
+		if(row){
+		
+		$('#dialog-m_retur').dialog({ 
+		    closed: false, 
+			cache: false, 
+			modal: true, 
+			href:base_url+'index.php/retur/formRetur',
+			title:'Edit Retur',
+			onLoad:function(){
+				
+				
+				$('#form_retur').form('clear');
+				$('#form_retur #edit').val('1');
+				$('#form_retur').form('load',row);	
+				$('#datagrid-m_detail').datagrid('load',{id_retur:row.id_retur});
+				
+			}
+			});
+		}else{
+			$.messager.alert('INFO','Pilih satu record dulu','info');
+		}
+		
+	}
+	
+	function simpanRetur(){
+		$.messager.progress({
+                title:'',
+                msg:'Simpan Retur...',
+				text:''
+         });
+			
+		$('#form_retur').form('submit',{
+			url: '<?php echo site_url('retur/simpanRetur'); ?>',
+			onSubmit: function(){ 
+				var isValid = $(this).form('validate');
+				if (!isValid){
+					$.messager.progress('close');
+					return $(this).form('validate');
+				}
+			},
+			success: function(result){
+				$.messager.progress('close');
+				var result = eval('('+result+')');
+					if(result.error){
+						$.messager.alert('INFO',result.msg,'info');
+					
+					}else{
+						$('#dialog-m_retur').dialog('close');
+						$('#datagrid-m_retur').datagrid('reload');
+						$.messager.alert('INFO',result.msg,'info');
+				
+					
+					}
+					
+			}
+		});
+	}
+	
+    function removeRetur(){
+		var row = $('#datagrid-m_retur').datagrid('getSelected');
+		if (row){
+			$.messager.confirm('Konfirmasi', 'Anda yakin menghapus data ini',function(r){
+				if (r){
+					$.post('<?php echo site_url('retur/removeRetur'); ?>',{id_retur:row.id_retur},function(result){
+						if (!result.error){
+							$('#datagrid-m_retur').datagrid('reload');
+							$.messager.alert('INFO',result.msg,'info');
+							} else {
+							$.messager.alert('INFO',result.msg,'info');
+						}
+					},'json');
+				}
+			});
+			}else{
+			$.messager.alert('INFO','Pilih satu record dulu','info');
+		}
+	}
+</script>
